18d006440282371ac322512fb4044e11c99e906d
Namo-k <121117705+Namo-k@users.noreply.github.com>
2024-01-10 03:16:43 +0100

    connexion add div info mdp

--- php/connexion.php
+++ php/connexion.php
@@ -71,8 +71,9 @@
                             </div>
                             <div class="group">
                                 <label for="pass" class="label">Mot de passe</label>
-                                <input id="pass" placeholder="Mot de passe" type="password" class="input" data-type="password" name="mdpInscr">
+                                <input id="passInscr" placeholder="Mot de passe" type="password" class="input" data-type="password" name="mdpInscr">
                             </div>
+                            <div id="passwordDiv"></div>
                             <div class="group">
                                 <input type="submit" class="button" value="S'inscrire" name="incription">
                             </div>
@@ -117,7 +118,72 @@
             document.title = newPageTitle;
             fav.setAttribute("href", "../images/icon2.png");
         }
+
+        document.addEventListener('DOMContentLoaded', function() {
+            var passwordInput = document.getElementById('passInscr');
+            var passwordConditions = document.createElement('div');
+            var submitButton = document.querySelector('[name="incription"]');
+
+            passwordConditions.className = 'password-conditions';
+            passwordConditions.innerHTML = 'Conditions du mot de passe : ';
+
+            passwordInput.parentNode.insertBefore(passwordConditions, passwordInput.nextSibling);
+
+            function updatePasswordConditions() {
+                var password = passwordInput.value;
+
+                var lengthCondition = (password.length >= 8 && password.length <= 20);
+                var specialCharCondition = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+                var digitCondition = /\d/.test(password);
+                var letterCondition = /[a-zA-Z]/.test(password);
+
+                passwordConditions.innerHTML = 'Conditions du mot de passe : ';
+
+                if (lengthCondition) {
+                    passwordConditions.innerHTML += '<span style="color: green;">Longueur valide</span>, ';
+                } else {
+                    passwordConditions.innerHTML += '<span style="color: red;">Longueur doit être entre 8 et 20 caractères</span>, ';
+                }
+
+                if (specialCharCondition) {
+                    passwordConditions.innerHTML += '<span style="color: green;">Caractères spéciaux présents</span>, ';
+                } else {
+                    passwordConditions.innerHTML += '<span style="color: red;">Caractères spéciaux requis</span>, ';
+                }
+
+                if (digitCondition) {
+                    passwordConditions.innerHTML += '<span style="color: green;">Chiffres présents</span>, ';
+                } else {
+                    passwordConditions.innerHTML += '<span style="color: red;">Chiffres requis</span>, ';
+                }
+
+                if (letterCondition) {
+                    passwordConditions.innerHTML += '<span style="color: green;">Lettres présentes</span>';
+                } else {
+                    passwordConditions.innerHTML += '<span style="color: red;">Lettres requises</span>';
+                }
+
+                if (lengthCondition && specialCharCondition && digitCondition && letterCondition) {
+                    submitButton.removeAttribute('disabled');
+                    submitButton.classList.remove('disabled-button');
+                } else {
+                    submitButton.setAttribute('disabled', 'disabled');
+                    submitButton.classList.add('disabled-button');
+                }
+            }
+
+            passwordInput.addEventListener('input', updatePasswordConditions);
+        });
     </script>
+
+    <style>
+        .disabled-button {
+            background-color: #ddd;
+            color: #888;
+            cursor: not-allowed;
+        }
+    </style>
+
 </body>
 
 </html>
