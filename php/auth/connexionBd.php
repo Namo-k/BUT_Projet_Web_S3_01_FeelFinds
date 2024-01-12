f6ce9798b46774ff0fa076a8306594bc5ed26f8e
Namo-k <121117705+Namo-k@users.noreply.github.com>
2024-01-13 00:07:57 +0100

    Delete php directory

--- php/auth/connexionBd.php
+++
@@ -1,4 +0,0 @@
-<?php
-$db = new PDO('mysql:host=localhost;dbname=bd_feelfinds', 'root', '');
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
--- php/include/barSentiment.php
+++
@@ -1,8 +0,0 @@
-<div class="rectangle">
-    <img src="../../images/emoji_epoustouflant.png" width="26px" class="emojis emoji_epoustouflant">
-    <img src="../../images/emoji_triste.png" width="26px" class="emojis emoji_triste">
-    <img src="../../images/emoji_amour.png" width="26px" class="emojis emoji_amour">
-    <img src="../../images/emoji_joyeux.png" width="26px" class="emojis emoji_joyeux">
-    <img src="../../images/emoji_emouvant.png" width="26px" class="emojis emoji_emouvant">
-    <img src="../../images/emoji_festif.png" width="26px" class="emojis emoji_festif">
-</div>
